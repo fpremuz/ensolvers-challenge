f38efd52a3a4d026002786e6a00c102bd19a3161
Francisco Premuz <fpremuz@gmail.com>
2022-03-26 19:40:29 -0300

    primeros pasos

--- ensolvers-challenge-backend/src/app.controller.ts
+++ ensolvers-challenge-backend/src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { AppService } from './app.service';
+import type { Todo } from './app.service';
 
-@Controller()
+@Controller('/api/todos')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getTodos(): Todo[] {
+    return this.appService.getTodos();
+  }
+
+  @Post()
+  addToDo(@Body() todo: Todo) {
+    this.appService.addToDo(todo);
+  }
+
+  @Put(':id')
+  replaceToDo(@Param('id') id: string, @Body() todo: Todo) {
+    this.appService.replaceToDo(id, todo);
   }
 }
--- ensolvers-challenge-backend/src/app.service.ts
+++ ensolvers-challenge-backend/src/app.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
+
+export type Todo = {
+  id?: string;
+  text: string;
+  isCompleted: boolean;
+};
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly todos: Todo[] = [];
+  getTodos(): Todo[] {
+    return this.todos;
+  }
+
+  addToDo(todo: Todo) {
+    this.todos.push({ ...todo, id: uuidv4() });
+  }
+
+  replaceToDo(id: string, todo: Todo) {
+    const asd = this.todos.find((t) => t.id === id);
+    if (asd === undefined) throw Error();
+    asd.isCompleted = todo.isCompleted;
+    asd.text = todo.text;
   }
 }
